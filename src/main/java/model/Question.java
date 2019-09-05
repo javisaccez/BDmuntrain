7e534fe7c0682e5111a5398b2b6cbee33ea6dcab
javisaccez <yyoquese_net@hotmail.com>
2019-09-05 17:21:50 +0200

    creadas las entidades

---
+++ src/main/java/model/Question.java
@@ -0,0 +1,55 @@
+package model;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.*;
+
+
+/**
+ * The persistent class for the question database table.
+ * 
+ */
+@Entity
+@Table (name = "question")
+@NamedQuery(name="Question.findAll", query="SELECT q FROM Question q")
+public class Question implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column (name = "id")
+	private int id;
+
+	@Lob
+	@Column(name = "description")
+	private String description;
+	
+	@OneToMany (mappedBy = "question", cascade = CascadeType.ALL)
+	private List<Responses>responses = new ArrayList<>();
+	
+	@ManyToOne (fetch = FetchType.EAGER)
+	@JoinColumn (name = "id_examtemplate")
+	private Examtemplate examtemplate;
+
+	public Question() {
+	}
+
+	public int getId() {
+		return this.id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getDescription() {
+		return this.description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+}
